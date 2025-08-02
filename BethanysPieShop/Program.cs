64905b036e5653d368a8d80429ee698100c78e15
Alex Khorozov <akhorozov@outlook.com>
2025-08-02 01:34:21 -0400

    Refactor ASP.NET Core setup and launch settings
    Updated Program.cs to utilize the minimal hosting model,
    adding MVC support and configuring middleware for static files
    and error handling. Removed the root URL mapping in favor of
    a default controller route.
    
    Modified launchSettings.json to eliminate the iisSettings
    section, removed the http profile, and streamlined the
    IIS Express profile to only include the https application URL.

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+//builder .Services.AddScoped<IloggerService, LoggerService>();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute();
 
 app.Run();
