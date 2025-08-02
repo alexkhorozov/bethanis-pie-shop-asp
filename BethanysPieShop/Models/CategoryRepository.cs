4fab1593ac81393642209c751f74701cbb9b1e37
Alex Khorozov <akhorozov@outlook.com>
2025-08-02 11:25:13 -0400

    Add EF Core support and refactor repositories
    Updated project to use Entity Framework Core with SQL Server, added UserSecretsId, and replaced mock repositories with actual implementations. Created DbContext and repository classes for managing categories and pies. Updated README title for clarity.

---
+++ BethanysPieShop/Models/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿
+namespace BethanysPieShop.Models;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly BethanysPieShopDbContext _context;
+
+    public CategoryRepository(BethanysPieShopDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Category> AllCategories =>
+        _context.Categories.OrderBy(p => p.CategoryName);
+
+    public Category? GetCategoryById(int categoryId) =>
+        _context.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
+}
